4e650211c13f35f0348c106201a7fe9277747837
Nikita Kulakov <nvkulakov.dev@gmail.com>
2025-06-03 11:25:03 +0300

    refactor

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -6,24 +6,60 @@ import (
 	"fmt"
 	"log"
 	"mals-engine/internal/jsonrpc"
-	"mals-engine/internal/logger"
 	"net"
+	"os"
 )
 
-type Context struct {
+type Params struct {
 	flagPort int
+}
+
+type Engine struct {
+	Params
 
 	logger *log.Logger
 }
 
-func (ctx Context) connHandle(conn net.Conn) {
-	defer func() {
-		conn.Close()
-		ctx.logger.Printf("info: client disconnected %s", conn.RemoteAddr())
-	}()
+func (self *Params) parse() {
+	flag.IntVar(&self.flagPort, "p", 9200, "port to serve")
 
-	ctx.logger.Printf("info: client connected %s", conn.RemoteAddr())
+	flag.Parse()
+}
 
+func (self *Engine) setupLogger() {
+	self.logger = log.New(os.Stdout, "", log.LUTC|log.Lshortfile|log.Ldate|log.Ltime)
+}
+
+func (self *Engine) listen() error {
+	listener, err := net.Listen("tcp", ":"+fmt.Sprint(self.flagPort))
+	if err != nil {
+		return err
+	}
+	self.logger.Println("info: listening on port " + fmt.Sprint(self.flagPort))
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			self.logger.Printf("error: %s", err)
+			continue
+		}
+
+		go func() {
+			self.logger.Printf("info: %s connected", conn.RemoteAddr())
+
+			defer func() {
+				self.logger.Printf("info: %s disconnected", conn.RemoteAddr())
+				conn.Close()
+			}()
+
+			self.connHandle(conn)
+		}()
+	}
+}
+
+func (self *Engine) connHandle(conn net.Conn) {
 	scanner := bufio.NewScanner(conn)
 	scanner.Split(jsonrpc.ScannerSplit)
 
@@ -33,51 +69,27 @@ func (ctx Context) connHandle(conn net.Conn) {
 		msg, _, err := jsonrpc.DecodeRequestMessage(bytes)
 
 		if err != nil {
-			ctx.logger.Printf("error: unable to decode %s", err)
+			self.logger.Printf("error: %s unable to decode %s", conn.RemoteAddr(), err)
 			continue
 		}
 
-		ctx.logger.Printf("info: handling method %s", msg.Method)
+		self.logger.Printf("info: %s handling method %s", conn.RemoteAddr(), msg.Method)
 
 		switch msg.Method {
 		case "initialize":
 			break
 		default:
-			ctx.logger.Printf("warn: unhandled method %s", msg.Method)
+			self.logger.Printf("warn: %s unhandled method %s", conn.RemoteAddr(), msg.Method)
 			break
 		}
 	}
 }
 
 func main() {
-	var ctx Context
+	var engine Engine
 
-	flag.IntVar(&ctx.flagPort, "p", 9200, "port to serve")
+	engine.parse()
+	engine.setupLogger()
 
-	flag.Parse()
-
-	logger, err := logger.GetLogger()
-	if err != nil {
-		panic(err)
-	}
-	ctx.logger = logger
-
-	ctx.logger.Println("info: started")
-
-	listener, err := net.Listen("tcp", ":"+fmt.Sprint(ctx.flagPort))
-	if err != nil {
-		panic(err)
-	}
-	defer listener.Close()
-
-	ctx.logger.Println("info: server listening on port " + fmt.Sprint(ctx.flagPort))
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			ctx.logger.Printf("error: %s", err)
-			continue
-		}
-		go ctx.connHandle(conn)
-	}
+	engine.listen()
 }
