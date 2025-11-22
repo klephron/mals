766b0596f77b4f9889f64cfa0875880b79639b18
Nikita Kulakov <klephron.dev@gmail.com>
2025-11-22 22:24:04 +0300

    add log, update directory structure

---
+++ internal/log/log.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+type Log struct {
+	logfile *os.File
+	logger  *slog.Logger
+}
+
+func logLevel(level int) slog.Level {
+	switch {
+	case level <= 0:
+		return slog.LevelError
+	case level <= 1:
+		return slog.LevelWarn
+	case level <= 2:
+		return slog.LevelInfo
+	default:
+		return slog.LevelDebug
+	}
+}
+
+func Open(path string, level int) (*Log, error) {
+	logfile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to open log file %s", path)
+	}
+
+	logger := slog.New(slog.NewTextHandler(logfile, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     logLevel(level),
+	}))
+
+	return &Log{logfile, logger}, nil
+}
+
+func (log *Log) Close() {
+	log.logfile.Close()
+}
