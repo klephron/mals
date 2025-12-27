40ba03767e910c1be05ec6900e5e041acc2e621e
Nikita Kulakov <klephron.dev@gmail.com>
2025-12-27 21:14:12 +0300

    rm unused

--- internal/.deprecated/handle.go
+++ internal/.deprecated/handle.go
@@ -8,69 +8,6 @@ package lsp
 // 	"mals/pkg/url"
 // )
 
-// func (s *Client) writeResponse(response any) {
-// 	if bytes, err := jsonrpc.Encode(response); err == nil {
-// 		s.writer.Write(bytes)
-// 		s.writer.Flush()
-// 	} else {
-// 		s.LogErrorPrintf("cannot encode message to send")
-// 	}
-// }
-
-// func (s *Client) textDocumentDidOpen(data []byte) {
-// 	var notification lsp_message.DidOpenTextDocumentNotification
-// 	json.Unmarshal(data, &notification)
-
-// 	fileUri := notification.Params.TextDocument.Uri
-// 	path, err := url.UriToPath(fileUri)
-// 	if err != nil {
-// 		s.LogErrorPrintf("unable to resolve file uri: %s", err)
-// 		return
-// 	}
-
-// 	if workspace, found := s.FindWorkspace(path); found {
-// 		workspace.OpenDocument(path, notification.Params.TextDocument.Text)
-// 		s.LogInfoPrintf("workspace %s: document %s: opened", workspace.Root, path)
-// 	}
-// }
-
-// func (s *Client) textDocumentDidChange(data []byte) {
-// 	var notification lsp_message.DidChangeTextDocumentNotification
-// 	json.Unmarshal(data, &notification)
-
-// 	fileUri := notification.Params.TextDocument.Uri
-// 	path, err := url.UriToPath(fileUri)
-// 	if err != nil {
-// 		s.LogErrorPrintf("unable to resolve file uri: %s", err)
-// 		return
-// 	}
-
-// 	if workspace, found := s.FindWorkspace(path); found {
-// 		for _, change := range notification.Params.ContentChanges {
-// 			// NOTE: assumes change == FULL
-// 			workspace.ChangeDocument(path, change.Text)
-// 			s.LogInfoPrintf("workspace %s: document %s: changed", workspace.Root, path)
-// 		}
-// 	}
-// }
-
-// func (s *Client) textDocumentDidClose(data []byte) {
-// 	var notification lsp_message.DidCloseTextDocumentNotification
-// 	json.Unmarshal(data, &notification)
-
-// 	fileUri := notification.Params.TextDocument.Uri
-// 	path, err := url.UriToPath(fileUri)
-// 	if err != nil {
-// 		s.LogErrorPrintf("unable to resolve file uri: %s", err)
-// 		return
-// 	}
-
-// 	if workspace, found := s.FindWorkspace(path); found {
-// 		workspace.CloseDocument(path)
-// 		s.LogInfoPrintf("workspace %s: document %s: closed", workspace.Root, path)
-// 	}
-// }
-
 // func (s *Client) textDocumentCompletion(data []byte) {
 // 	var request lsp_message.CompletionRequest
 // 	json.Unmarshal(data, &request)
