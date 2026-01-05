45f9bb18c961a3311b6de0e4f4a051608ea92f7c
Nikita Kulakov <klephron.dev@gmail.com>
2026-01-05 18:11:16 +0300

    update version

--- pkg/info/middleware.go
+++ pkg/info/middleware.go
@@ -3,5 +3,5 @@ package info
 const (
 	MiddlewareServerName string = "mals"
 	MiddlewareClientName string = "mals-middleware"
-	MiddlewareVersion    string = "0.0.1"
+	MiddlewareVersion    string = "0.1.0"
 )
