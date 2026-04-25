2bf6df1ad19039c7d759bf51dbd7c138d18ab97d
Nikita Kulakov <klephron.dev@gmail.com>
2026-04-25 19:43:01 +0300

    add cfg building

--- internal/middleware/handler/textdocumentcompletion.go
+++ internal/middleware/handler/textdocumentcompletion.go
@@ -8,6 +8,20 @@ import (
 	"strings"
 )
 
+type executionNode struct {
+	Step *config.Step
+	Then *executionNode
+	Else *executionNode
+}
+
+type executionContext struct {
+	graph *executionNode
+	// resources
+	// get (dict)
+	// set (dict)
+	// file variables
+}
+
 func (s *Handler) TextDocumentCompletionDefault(params *protocol.CompletionParams) (*protocol.CompletionList, error) {
 	list := protocol.CompletionList{
 		IsIncomplete: false,
@@ -61,6 +75,113 @@ func (s *Handler) TextDocumentCompletionDefault(params *protocol.CompletionParam
 	})
 
 	return &list, nil
+}
+
+// recursively builds CFG (cuz execution graph is rather small no optimizations are done)
+// returns (start, end pair execution)
+// if: Then - then, Else - else
+// while: Then - do, Else - <statement_after>
+// simple: Then - <statement_after>
+func (s *Handler) executionBuild(steps []*config.Step, i int) (*executionNode, *executionNode) {
+	if i >= len(steps) {
+		return nil, nil
+	}
+
+	curr := steps[i]
+
+	switch cd := curr.Definition.(type) {
+	case *config.StepIf:
+		execThenStart, execThenEnd := s.executionBuild(cd.Then, 0)
+		execElseStart, execElseEnd := s.executionBuild(cd.Else, 0)
+
+		if execThenStart == nil {
+			s.plane.Errorf("%T %v: TextDocumentCompletion step %v then is nil", s, s.Name(), curr)
+			return nil, nil
+		}
+
+		execCurrStart := &executionNode{
+			Step: curr,
+			Then: execThenStart,
+			Else: execElseStart,
+		}
+
+		execNextStart, execNextEnd := s.executionBuild(steps, i+1)
+
+		if execThenEnd != nil {
+			execThenEnd.Then = execNextStart
+		}
+		if execElseEnd != nil {
+			execElseEnd.Then = execNextStart
+		}
+
+		execCurrEnd := execNextEnd
+		if execCurrEnd == nil {
+			execCurrEnd = execThenEnd // any branch is valid
+		}
+		return execCurrStart, execCurrEnd
+
+	case *config.StepWhile:
+		execDoStart, execDoEnd := s.executionBuild(cd.Do, 0)
+
+		if execDoStart == nil {
+			s.plane.Errorf("%T %v: TextDocumentCompletion step %v do is nil", s, s.Name(), curr)
+			return nil, nil
+		}
+
+		execNextStart, execNextEnd := s.executionBuild(steps, i+1)
+
+		execCurrStart := &executionNode{
+			Step: curr,
+			Then: execDoStart,
+			Else: execNextStart,
+		}
+
+		if execDoEnd != nil {
+			execDoEnd.Then = execCurrStart
+		}
+
+		execCurrEnd := execNextEnd
+		if execCurrEnd == nil {
+			execCurrEnd = execCurrStart // loop re-entry
+		}
+
+		return execCurrStart, execCurrEnd
+
+	case *config.StepReturn:
+		execCurrStart := &executionNode{
+			Step: curr,
+			Then: nil,
+			Else: nil,
+		}
+
+		return execCurrStart, execCurrStart
+
+	case *config.StepJsonDumps:
+	case *config.StepJsonParse:
+	case *config.StepJsonParseCompletion:
+	case *config.StepLspCompetion:
+	case *config.StepModelSimple:
+	case *config.StepModelTemplate:
+	default:
+		s.plane.Errorf("%T %v: unexpected config.StepDefinition: %#v", s, s.Name(), cd)
+		return nil, nil
+	}
+
+	// handle simple statements
+	execNextStart, execNextEnd := s.executionBuild(steps, i+1)
+
+	execCurrStart := &executionNode{
+		Step: curr,
+		Then: execNextStart,
+		Else: nil,
+	}
+
+	execCurrEnd := execNextEnd
+	if execCurrEnd == nil {
+		execCurrEnd = execCurrStart
+	}
+
+	return execCurrStart, execCurrEnd
 }
 
 func (s *Handler) TextDocumentCompletionCustom(params *protocol.CompletionParams) (*protocol.CompletionList, error) {
